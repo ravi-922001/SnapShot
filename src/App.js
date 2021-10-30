9971079f1196bbb2eb1eae9a09b5657bbaac51d9
Goyal2001-30 <official.ravish.92@gmail.com>
2021-10-30 09:36:43 +0530

    App completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react'
 import './App.css';
-import { photoData } from './photoData';
 import Picture from './Picture';
 
 function App() {
-  
-  const [imageData, setImageData] = useState(photoData);
+  const [search, setSearch] = useState('');
+  const [imageData, setImageData] = useState([]);
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    const client_ID = 'YDOem0iNmXcuLHk0ZM4UA4DimOsW3ThTRDJJccqcf7E';
+    axios.get(`https://api.unsplash.com/search/photos/?query=`+search+`&per_page=49&client_id=${client_ID}`)
+    .then(res => {
+      console.log(res.data.results);
+      setImageData(res.data.results);
+    });
+  }, [search])
 
   return (
     <div className="App">
-      <div className="snapshot">
-        {imageData.map(data => <Picture title={data.title} link={data.link} />)}
+      <div className="navbar">
+        SnapShot
       </div>
+      <div className="input">
+        <input placeholder="Search here" onChange={searchHandler} value={search} />
+      </div>
+      <div className="snapshot">
+        {imageData.map(data => <Picture key={data.id} title={data.alt_description} link={data.urls.regular} />)}
+      </div>
+    
     </div>
   );
 }
